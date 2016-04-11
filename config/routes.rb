1882151e3e327b54036778c5c0e3adfbfe63bd7a
javislaterlp <javierho93@hotmail.com>
2016-04-11 12:38:17 +0100

    Localization

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
+
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
@@ -10,16 +11,18 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
 
   get 'store/index'
   resources :products do
      get :who_bought, on: :member
   end
 
-  root 'store#index', as: 'store'
+  scope '(:locale)' do
+     resources :orders
+     resources :line_items
+     resources :carts
+     root 'store#index', as: 'store', via: :all
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
